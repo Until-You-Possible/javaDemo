fc45710d1bc7969ca656c8a473e51c8541ff412a
Gang Wang <gang.wang05@sap.com>
2021-12-22 14:48:50 +0800

    X

--- src/main/java/com/example/demo/controller/SendMail.java
+++ src/main/java/com/example/demo/controller/SendMail.java
@@ -1,29 +1,49 @@
 package com.example.demo.controller;
 
-import com.example.demo.service.MailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @RestController
 @RequestMapping
 public class SendMail {
 
-    private final MailService mailService;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public SendMail(MailService mailService) {
-        this.mailService = mailService;
-    }
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
 
     @GetMapping("/sendMailTest")
-    public String sendmailToWg() {
-        // String to, String subject, String content
-        String to = "arthurwanggang@yeah.net";
-        String content = "this is mail test text";
-        String subject = "this is subject title";
-
-        mailService.sendSimpleTextMail(to, subject, content);
-        return "邮件已发送";
+    public void sendmailToWg() {
+        this.sendSimpleTextMail();
+    }
+    /**
+     * 发送简单文本邮件
+     *
+     * @param to
+     * @param subject
+     * @param content
+     */
+    public void sendSimpleTextMail() {
+        //简单邮件
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("admin@163.com");
+        simpleMailMessage.setTo("arthurwanggang@outlook.com");
+        simpleMailMessage.setSubject("Happy New Year");
+        simpleMailMessage.setText("新年快乐！");
+        javaMailSender.send(simpleMailMessage);
     }
 
 }
--- src/main/java/com/example/demo/service/MailService.java
+++ src/main/java/com/example/demo/service/MailService.java
@@ -1,13 +1,20 @@
 package com.example.demo.service;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
 
 @Service
 public class MailService {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Value("${spring.mail.username}")
     private String from;
@@ -25,11 +32,5 @@ public class MailService {
      * @param content
      */
     public void sendSimpleTextMail(String to, String subject, String content) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(content);
-        message.setFrom(from);
-        mailSender.send(message);
     }
 }
